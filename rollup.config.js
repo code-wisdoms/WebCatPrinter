4556078509077ca6bae9cc3cc32da298375eb41f
CodeWisdoms <ahmed.r.qadri@gmail.com>
2024-06-12 09:35:18 +0500

    rollup config update

--- rollup.config.js
+++ rollup.config.js
@@ -7,17 +7,20 @@ module.exports = {
       file: "dist/bundle.cjs.js",
       format: "cjs",
       sourcemap: true,
+      banner: `/*\n* Web Cat Printer\n* (c) 2023-${new Date().getFullYear()} Ahmed Raza\n* Cannot use, copy, modify, and/or distribute this software for any purpose with or without fee.\n*/`
     },
     {
       file: "dist/bundle.esm.js",
       format: "esm",
       sourcemap: true,
+      banner: `/*\n* Web Cat Printer\n* (c) 2023-${new Date().getFullYear()} Ahmed Raza\n* Cannot use, copy, modify, and/or distribute this software for any purpose with or without fee.\n*/`
     },
     {
       file: "dist/bundle.umd.js",
       format: "umd",
       name: "MyLibrary",
       sourcemap: true,
+      banner: `/*\n* Web Cat Printer\n* (c) 2023-${new Date().getFullYear()} Ahmed Raza\n* Cannot use, copy, modify, and/or distribute this software for any purpose with or without fee.\n*/`
     },
   ],
   plugins: [typescript()],
